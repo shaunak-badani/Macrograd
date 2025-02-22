f24fe38af7440bcbc3a4aa9be43fe82717c5da35
Shaunak Badani <shaunak.badani@duke.edu>
2025-02-21 23:51:50 -0500

    Cleanup

--- datasetreader/DataSetsPathConfig.h
+++
@@ -1,9 +0,0 @@
-#ifndef DATASETS_PATH_CONFIG_H
-#define DATASETS_PATH_CONFIG_H
-
-#include <iostream>
-#include <string>
-
-const std::string DATASETS_PATH = "/home/shaunak/Work/PostCollegeStuff/Macrograd/datasets";
-
-#endif
